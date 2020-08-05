f26269da420f9226bbaf44351cd4673088428bee
JHB-hi <66860005+JHB-hi@users.noreply.github.com>
2020-08-05 19:10:57 +0900

    Update index.js

--- day1/index.js
+++ day1/index.js
@@ -1,16 +1,6 @@
 "use strict";
 
 const title = document.querySelector(".title");
-const bg = document.getElementsByTagName("body");
-
-// title.innerHTML = "The mouse is here!";
-// title.style.color = "red";
-// title.innerHTML = "The mouse is gone!";
-// title.style.color = "blue";
-// title.innerHTML = "You just resized!";
-// title.style.color = "orange";
-// title.innerHTML = "That was a right click!";
-// title.style.color = "green";
 
 function superTitleMouseover() {
   title.innerHTML = "The mouse is here!";
