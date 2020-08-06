b91ee0cf6a305919313938efc892ca0c87dfd19b
JHB-hi <66860005+JHB-hi@users.noreply.github.com>
2020-08-06 17:22:53 +0900

    little fix

--- day1/index.js
+++ day1/index.js
@@ -1,28 +1,55 @@
 "use strict";
 
+const color = ["red", "blue", "orange", "green"];
 const title = document.querySelector(".title");
 
-function superTitleMouseover() {
-  title.innerHTML = "The mouse is here!";
-  title.style.color = "red";
+const superTitle = {
+  mouseover: function () {
+    title.innerHTML = "The mouse is here!";
+    title.style.color = color[0];
+  },
+
+  mouseLeave: function () {
+    title.innerHTML = "The mouse is gone!";
+    title.style.color = color[1];
+  },
+
+  resize: function () {
+    title.innerHTML = "You just resized!";
+    title.style.color = color[2];
+  },
+
+  auxclick: function () {
+    title.innerHTML = "That was a right click!";
+    title.style.color = color[3];
+  },
+};
+
+// function superTitleMouseover() {
+//   title.innerHTML = "The mouse is here!";
+//   title.style.color = color[0];
+// }
+
+// function superTitleMouseleave() {
+//   title.innerHTML = "The mouse is gone!";
+//   title.style.color = color[1];
+// }
+
+// function superTitleRisize() {
+//   title.innerHTML = "You just resized!";
+//   title.style.color = color[2];
+// }
+
+// function superTitleAuxclick() {
+//   title.innerHTML = "That was a right click!";
+//   title.style.color = color[3];
+// }
+
+function init() {
+  title.addEventListener("mouseover", superTitle.mouseover);
+  title.addEventListener("mouseleave", superTitle.mouseLeave);
+  window.addEventListener("resize", superTitle.resize);
+  window.addEventListener("auxclick", superTitle.auxclick);
 }
 
-function superTitleMouseleave() {
-  title.innerHTML = "The mouse is gone!";
-  title.style.color = "blue";
-}
-
-function superTitleRisize() {
-  title.innerHTML = "You just resized!";
-  title.style.color = "orange";
-}
-
-function superTitleAuxclick() {
-  title.innerHTML = "That was a right click!";
-  title.style.color = "green";
-}
-
-title.addEventListener("mouseover", superTitleMouseover);
-title.addEventListener("mouseleave", superTitleMouseleave);
-window.addEventListener("auxclick", superTitleAuxclick);
-window.addEventListener("resize", superTitleRisize);
+init();
